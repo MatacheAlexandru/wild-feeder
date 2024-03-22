8aca0a32a5d62924ab47ed089af4dbdb664dfe03
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-03-22 20:28:09 +0200

    testA

--- java-script/h1.js
+++ java-script/h1.js
@@ -1,7 +1,22 @@
-document.getElementById("menuButton").addEventListener("click", function() {
-  var dropdown = document.getElementById("dropdownContent");
-  dropdown.classList.toggle("show");
-  
-  var icon = document.getElementById("menuIcon");
-  icon.classList.toggle("rotate");
+document.addEventListener("DOMContentLoaded", function() {
+  const menuButton = document.getElementById("menuButton");
+  const dropdownContent = document.getElementById("dropdownContent");
+
+  menuButton.addEventListener("click", function(event) {
+    event.stopPropagation(); // Oprim propagarea evenimentului pentru a preveni închiderea meniului când se face clic pe buton
+    menuButton.classList.toggle("rotate");
+    dropdownContent.classList.toggle("show");
+    if (dropdownContent.classList.contains("show")) {
+      dropdownContent.style.maxHeight = dropdownContent.scrollHeight + "px";
+    } else {
+      dropdownContent.style.maxHeight = "0";
+    }
+  });
+
+  document.addEventListener("click", function(event) {
+    if (!dropdownContent.contains(event.target)) {
+      dropdownContent.classList.remove("show");
+      dropdownContent.style.maxHeight = "0";
+    }
+  });
 });
